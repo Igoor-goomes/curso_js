f73587a0a363307465a260d1b5d7e63ff47ecd03
Igor Gomes <igorgomesdebrito.dev@gmail.com>
2024-01-27 18:47:40 -0300

    Consolidando JS

---
+++ aula_01/ex_01.js
@@ -0,0 +1,4 @@
+// Atividade -> deve apresentar o seguinte texto no console (Meu nome é "NOME". Estou aprendendo Javascript às 18:00 da tarde de Sábado)
+
+console.log(`Meu nome é "Igor Gomes". Estou aprendendo Javascript às 18:00 da tarde de Sábado`); // Feito por mim 
+console.log('Meu nome é "Igor Gomes". Estou aprendendo Javascript às', 10, 'da manhã de Sábado'); // Feito pelo professor
---
+++ aula_01/index.js
@@ -0,0 +1,8 @@
+console.log('Olá Mundo'); // String = texto
+console.log("Olá 'Mundo'");
+console.log(`"Olá" 'Mundo'`);  
+
+console.log(123456); // number = números reais
+console.log(123.4823); // number - ponto flutuante
+
+console.log(27, 172.15, 'Igor Gomes de BRito');
